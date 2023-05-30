ced89dd67d50d7d554ec0dcfca7f1ae6a9ecaf64
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-05-30 19:49:55 +0200

    Lesing av arkiv fra fil

--- prototype/ancep/app/api/archive/route.ts
+++ prototype/ancep/app/api/archive/route.ts
@@ -1,23 +1,26 @@
 import { NextResponse } from 'next/server';
 import WordExtractor from 'word-extractor';
 import fs from 'fs';
+import path from 'path';
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const doc = searchParams.get('doc');
+    const filePath = path.join(process.cwd(), '/lib/archive');
+
+    console.log('Filepath: ', filePath);
 
     console.log('Search params: ', searchParams);
 
     if(!doc) {
-        const files = fs.readdirSync("lib/archive");
-        
+        const files = fs.readdirSync(filePath);
         console.log('Files fetch');
 
         return NextResponse.json(files);
     } 
 
     const extractor = new WordExtractor();
-    const extracted = await extractor.extract("lib/archive/" + doc);    
+    const extracted = await extractor.extract(filePath + "/" + doc);    
 
     return NextResponse.json(extracted.getBody());
 }
