9c350786a4c16446f1e2d85fc4296a43979ea5e0
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-05-30 13:00:45 +0200

    Test login google

--- prototype/ancep/lib/auth.ts
+++ prototype/ancep/lib/auth.ts
@@ -1,15 +1,16 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { AuthOptions } from 'next-auth/core/types';//  clientId: process.env.GOOGLE_ID as string,
-//  clientSecret: process.env.GOOGLE_SECRET as string,
+import { AuthOptions } from 'next-auth/core/types';
 
+// clientId: '11742604971-4loscbibrae5ak24t2lj5t133e15q3ag.apps.googleusercontent.com',
+// clientSecret: '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'
 
 export const authOptions: AuthOptions = {
  providers: [
   GoogleProvider({
   name: 'google',
-    clientId: '11742604971-4loscbibrae5ak24t2lj5t133e15q3ag.apps.googleusercontent.com',
-    clientSecret: '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'
+   clientId: process.env.GOOGLE_ID as string,
+   clientSecret: process.env.GOOGLE_SECRET as string,
   }),
  ],
  session: {
--- prototype/ancep/next.config.js
+++ prototype/ancep/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     darkmode: false,
+    env: {
+        GOOGLE_ID: '11742604971-4loscbibrae5ak24t2lj5t133e15q3ag.apps.googleusercontent.com',
+        GOOGLE_SECRET: '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'
+    },
     images: {
         remotePatterns: [
 
