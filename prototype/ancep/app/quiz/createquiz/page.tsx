891d85488d686a7af4bbfdea3181117d5f5d83d7
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-05-30 19:33:27 +0200

    Quiz

--- prototype/ancep/app/quiz/createquiz/page.tsx
+++ prototype/ancep/app/quiz/createquiz/page.tsx
@@ -28,23 +28,25 @@ export default function createQuiz(){
     //     return elements;
     // }
 
-    return(
-        <div className="flex flex-col p-1" >
-            <input type="text" value={quiz?.Name}  className=" rounded border-black border border-[1px] mb-1"/>
-            <ul>
-                <li className="flex flex-col border rounded border-black">
-                    <div className="flex">
-                        <label>Spørsmåls type:</label>
-                        <select title="Spørsmåls type">
-                        </select>
-                    </div>
-                    <div className="flex flex-col">
-                        <label>Spørsmål:</label>
-                        <input type="text rounded border border-black border-[1px]"></input>
-                    </div>
-                </li>
-                <button className="bg-blue-500 w-[50px] rounded mt-2">Ny</button>
-            </ul>
-        </div>
-    )
+    // return(
+    //     <div className="flex flex-col p-1" >
+    //         <input type="text" value={quiz?.Name}  className=" rounded border-black border border-[1px] mb-1"/>
+    //         <ul>
+    //             <li className="flex flex-col border rounded border-black">
+    //                 <div className="flex">
+    //                     <label>Spørsmåls type:</label>
+    //                     <select title="Spørsmåls type">
+    //                     </select>
+    //                 </div>
+    //                 <div className="flex flex-col">
+    //                     <label>Spørsmål:</label>
+    //                     <input type="text rounded border border-black border-[1px]"></input>
+    //                 </div>
+    //             </li>
+    //             <button className="bg-blue-500 w-[50px] rounded mt-2">Ny</button>
+    //         </ul>
+    //     </div>
+    // )
+
+    return <h1>Quiz</h1>
 }
