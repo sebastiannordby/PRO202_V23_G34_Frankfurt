28ea2bab83ca7bdbeb0f1fbdb03ad18e8adf7ebf
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-05-30 13:16:26 +0200

    Test

--- prototype/ancep/next.config.js
+++ prototype/ancep/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     darkmode: false,
     env: {
         GOOGLE_ID: '11742604971-4loscbibrae5ak24t2lj5t133e15q3ag.apps.googleusercontent.com',
-        GOOGLE_SECRET: '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'
+        GOOGLE_SECRET: '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',
+        NEXTAUTH_SECRET: 'test'
     },
     images: {
         remotePatterns: [
