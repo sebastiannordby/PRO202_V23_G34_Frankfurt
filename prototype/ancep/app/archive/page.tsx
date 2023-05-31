237aa5e5c83ff5b58a4fd1257acff8e97b4da50f
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-05-31 12:02:43 +0200

    Lesing av arkiv dokumenter

--- prototype/ancep/app/archive/page.tsx
+++ prototype/ancep/app/archive/page.tsx
@@ -63,25 +63,24 @@ export default function ArchivePage() {
             </div>
 
             <div id="defaultModal" tabIndex={-1} aria-hidden={!documentVisible} 
-                className="transition-opacity fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative w-full max-w-2xl max-h-full">
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                            {document?.name}
-                        </h3>
-                        {/* <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            <span className="sr-only">Close modal</span>
-                        </button>
-                    */}
+                className="transition-opacity fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-hidden md:inset-0 backdrop-blur-md max-h-full h-screen  mx-auto">
+                <div className="relative h-full w-full max-w-2xl max-h-full mx-auto overflow-hidden">
+                    <div className="flex flex-col h-full overflow-hidden first-letter:relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <div className="flex items-start overflow-hidden justify-between p-4 border-b rounded-t dark:border-gray-600">
+                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                {document?.name?.replace('.doc', '')}
+                            </h3>
+                            {/* <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                        */}
 
                         </div> 
-                    
-                        <div className="p-6 space-y-6">
+                        <div className="p-6 space-y-6 h-full overflow-scroll flex-grow">
                             {document?.text}
                         </div>
-                        <div className="flex items-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                        <div className="flex items-end p-4 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
                             <button 
                                 data-modal-hide="defaultModal" 
                                 type="button" 
