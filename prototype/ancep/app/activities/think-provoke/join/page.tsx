50e2fd727ac7459f1edb2f5ae1c79a7d2f1336aa
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-06-02 23:36:12 +0200

    Tankevekker bli med side

--- prototype/ancep/app/activities/think-provoke/join/page.tsx
+++ prototype/ancep/app/activities/think-provoke/join/page.tsx
@@ -11,18 +11,18 @@ export default function JoinThinkProvokePage() {
 
                 <div>
                     <h3 className="text-lg">Tast inn tilgangskode</h3>
-                    <div className="flex gap-2">
+                    <div className="flex gap-2 mt-2">
                         <input 
-                            className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
-                            focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                            focus:border-blue-500 block flex-grow p-2.5 dark:bg-gray-700 dark:border-gray-600 
                             dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Tilgangskode"/>
                         <button 
                             data-modal-hide="defaultModal" 
                             type="button" 
                             className="ml-auto text-white bg-pink-600 hover:bg-pink-600 focus:ring-4 
-                                focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5
-                                py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
+                                focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm
+                                p-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
                                 dark:focus:ring-blue-800">Bli med</button>
                     </div>
                 </div>
