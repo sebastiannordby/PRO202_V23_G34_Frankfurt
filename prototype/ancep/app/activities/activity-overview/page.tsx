62eb59e36dc9c7a5508dead07b8c435987686ae3
Zaphand <andreas@halseth.no>
2023-06-07 15:25:38 +0200

    quiz view

--- prototype/ancep/app/activities/activity-overview/page.tsx
+++ prototype/ancep/app/activities/activity-overview/page.tsx
@@ -11,37 +11,6 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function ActivityOverview(){
-
-    const [quizes, setQuizes] = useState<Quiz[]>([]);
-    
-    const {status, data:session} = useSession();
-
-    useEffect(()=>{
-
-        (async()=>{
-            
-            console.log("This runs");
-            var data = await QuizService.all();
-            console.log(data);
-            setQuizes(data);
-
-        })();
-
-        var test:Quiz = new Quiz();
-        test._id = "test";
-        test.Name = "Min nye test quiz"
-
-        var testList:Quiz[] = [];
-
-        for (let i = 0; i < 10; i++) {
-            testList.push(test);
-        }
-
-
-
-    },[])
-
-
     return (
         <div className="flex flex-col main-layout">
             <div className="w-full bg-white bg-opacity-[0.7] p-4 rounded-[1.5rem] h-full overflow-hidden max-h-full">
@@ -52,7 +21,7 @@ export default function ActivityOverview(){
                     </Link>
                 </div>
 
-                <QuizListView quizes={quizes}/>
+                <QuizListView/>
             </div>
         </div>
     )
--- prototype/ancep/app/components/quiz/QuizListView.tsx
+++ prototype/ancep/app/components/quiz/QuizListView.tsx
@@ -3,35 +3,37 @@ import { useEffect, useState } from "react";
 import QuizOverView from "./QuizOverView";
 import { QuizService } from "@/lib/services/quizService";
 
-export default function QuizListView(props:{quizes:Quiz[]}){
-    const {quizes} = props;
+export default function QuizListView(){
 
     const [quizView, setQuizView]  = useState<JSX.Element[]>();
+
 
 
     const removeQuiz = async (_quiz:Quiz)=>{
 
         await QuizService.remove(_quiz._id ?? "");
-
+        await refreshQuizes();
     }
 
-    const refreshQuizes = ()=>{
-        
-    }
+    const refreshQuizes = async ()=>{
+        var quiz = await QuizService.all();
 
-    useEffect(()=>{
-    
-        console.log("QuizListView:");
-        console.log(quizes);
-        var quizViewValue = quizes?.map((data)=>{
+        console.log(quiz);
+        var quizViewValue = quiz?.map((data)=>{
             var key = (Math.random() * 10).toString();
             return(
                 <QuizOverView key={key} quiz={data} removeQuizClicked={removeQuiz}/>
             )
         })
         setQuizView(quizViewValue);
+    }
+
+    useEffect(()=>{
+        (async()=>{
+            await refreshQuizes();
+        })()
         
-    },[quizes])
+    },[])
 
     
 
